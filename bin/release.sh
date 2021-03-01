a19069029796859320d677e798b163bd2eba3c50
Tony Vashevko <avashevko@gmail.com>
2021-03-01 14:30:31 +0800

    add zip release creator

---
+++ bin/release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+zip survey-files \
+    ./survey/Network_Survey.qsf \
+    ./survey/panel_template.xlsx \
+    ./survey/panel_template.csv \
+    ./sample/ZKC\ Qualtrics.zip \
+    ./sample/zkc-qualtrics-panel.xlsx \
+    ./sample/zkc-qualtrics-panel.csv \
+    ./sample/ZKC\ Reports.zip
