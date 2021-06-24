3ebf83bf456c5af882257e57beae199a2385a803
Tony Vashevko <avashevko@gmail.com>
2021-06-24 17:43:36 +0800

    more flexible run/deploy scripts

--- bin/deploy.R
+++ bin/deploy.R
@@ -1,4 +1,27 @@
 #!/usr/bin/env Rscript
 
-rsconnect::deployApp('./app',
-                     appName = "ob2-survey")
+
+args <- commandArgs(trailingOnly=TRUE)
+
+apps <- list(
+    `app` = list(local = "./app", remote = "ob2-survey"),
+    `app-dev` = list(local = "./app", remote = "ob2-survey-dev"),
+    `report-app` = list(local = "./report-app", remote = "ob2-reports"),
+    `demo-app` = list(local = "./demo-app", remote = "ob2-demo")
+)
+
+if(length(args) > 0) {
+    app.arg <- args[1]
+    if(app.arg %in% names(apps)) {
+        local.app <- apps[[app.arg]]$local
+        remote.app <- apps[[app.arg]]$remote
+    } else {
+        stop(paste('app must be one of "', paste(names(apps), collapse = '" "'), '"\n', sep = ''))
+    }
+} else {
+    local.app <- apps[["app"]]$local
+    remote.app <- apps[["app"]]$remote
+}
+
+rsconnect::deployApp(local.app,
+                     appName = remote.app)
--- bin/run.R
+++ bin/run.R
@@ -1,3 +1,22 @@
 #!/usr/bin/env Rscript
 
-shiny::runApp('./app')
+args <- commandArgs(trailingOnly=TRUE)
+
+apps <- list(
+    `app` = './app',
+    `report-app` = './report-app',
+    `demo-app` = './demo-app'
+)
+
+if(length(args) > 0) {
+    app.arg <- args[1]
+    if(app.arg %in% names(apps)) {
+        dir.app <- apps[[app.arg]]
+    } else {
+        stop(paste('app must be one of "', paste(names(apps), collapse = '" "'), '"\n', sep = ''))
+    }
+} else {
+    dir.app <- apps[['app']]
+}
+
+shiny::runApp(dir.app)
